e036ef1ee1a52624f77820f40bc15800ffe9f92f
season1220 <ruturajubale35@gmail.com>
2025-12-06 14:28:16 +0530

    SEASON.PY

--- shivu/config.py
+++ shivu/config.py
@@ -1,27 +1,25 @@
 class Config(object):
     LOGGER = True
 
-    # Aapki Owner ID
+
     OWNER_ID = "7164618867"
     sudo_users = "7164618867"
 
-    TOKEN = "8578752843:AAE4IkimnlPzIHLs0K8zG3SZareZFry5g-g"
+    TOKEN = "8578752843:AAHNWJAKLmZ_pc9tHPgyhUtnjOKxtXD6mM8"
     
-    # Aapka Database Link
+
     mongo_url = "mongodb+srv://seasonking:admin123@cluster0.e5zbzap.mongodb.net/?appName=Cluster0"
-    
-    # Jahan Characters Upload honge (Channel ID)
+
     CHARA_CHANNEL_ID = "-1003337112485"
     GROUP_ID = -1003337112485
     
-    # Aapka Naya Bot Username
-    BOT_USERNAME = "seasonwaifuBot"
     
-    # Aapki API Details
+    BOT_USERNAME = "seasonwaifuBot"
+
     api_id = 34967775
     api_hash = "e6e5dfae5327f90410863f93d8ced26b"
 
-    # Extra Settings (Inhe aise hi rehne dein)
+ 
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "seasonwaifuBot"
     UPDATE_CHAT = "seasonwaifuBot"
