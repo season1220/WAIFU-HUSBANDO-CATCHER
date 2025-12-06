40c1f8c297b722c4d3063d40591dc7833620d474
season1220 <ruturajubale35@gmail.com>
2025-12-06 14:06:11 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -1,24 +1,33 @@
 class Config(object):
     LOGGER = True
 
-    # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    # Aapki Owner ID
+    OWNER_ID = "7164618867"
+    sudo_users = "7164618867"
 
+    TOKEN = "8578752843:AAE4IkimnlPzIHLs0K8zG3SZareZFry5g-g"
     
+    # Aapka Database Link
+    mongo_url = "mongodb+srv://seasonking:admin123@cluster0.e5zbzap.mongodb.net/?appName=Cluster0"
+    
+    # Jahan Characters Upload honge (Channel ID)
+    CHARA_CHANNEL_ID = "-1003337112485"
+    GROUP_ID = -1003337112485
+    
+    # Aapka Naya Bot Username
+    BOT_USERNAME = "seasonwaifuBot"
+    
+    # Aapki API Details
+    api_id = 34967775
+    api_hash = "e6e5dfae5327f90410863f93d8ced26b"
+
+    # Extra Settings (Inhe aise hi rehne dein)
+    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
+    SUPPORT_CHAT = "seasonwaifuBot"
+    UPDATE_CHAT = "seasonwaifuBot"
+
 class Production(Config):
     LOGGER = True
-
 
 class Development(Config):
     LOGGER = True
